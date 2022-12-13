c46ca948d57f351a78b2cd1a37e237d4557e94a5
balla999sandhya@gmail.com <balla999sandhya@gmail.com>
2022-12-13 18:02:57 +0530

    updated code

--- src/test/java/tests/Creation_Of_Template.java
+++ src/test/java/tests/Creation_Of_Template.java
@@ -33,28 +33,10 @@ import org.testng.annotations.Test;
 	    		+ "    \"transactionId\": \"6cd4fa3a-055b-4438-8296-4aa234ea1136\",\n"
 	    		+ "    \"additionalProperties\": {}\n"
 	    		+ "}";
-//  @BeforeAll
-//	    public static void setup() {
-//	        RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
-//	    }
 
 	    @Test
 	   
-//	        Response response = given()
-//	                .header("Content-type", "application/json")
-//	                .and()
-//	                .body(requestBody)
-//	                .when()
-//	                .post("https://ingress-gateway.gaiansolutions.com/template-service/v1.0/admin/admin/templates")
-//	                .then()
-//	                .extract().response();
-//	        System.out.println("Response body is" + requestBody.toString());
-//
-//	        Assert.assertEquals(201, response.statusCode());
-//	        Assert.assertEquals("foo", response.jsonPath().getString("title"));
-//	        Assert.assertEquals("bar", response.jsonPath().getString("body"));
-//	        Assert.assertEquals("1", response.jsonPath().getString("userId"));
-//	        Assert.assertEquals("101", response.jsonPath().getString("id"));
+
 	    public void postRequest() {
 	    Response postResponse=given()
 	    .auth().none()
